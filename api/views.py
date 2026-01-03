e3cb1aac073e6a05407890a9935480cc0d942f82
brandonloxton5-cmyk <brandonloxton5@gmail.com>
2026-01-03 22:30:40 +0200

    Update views.py

--- api/views.py
+++ api/views.py
@@ -8,3 +8,7 @@ def health(request):
 @api_view(["GET"])
 def hello(request):
     return Response({"message": "Bobbies Rugby API is running"})
+from django.http import JsonResponse
+
+def health(request):
+    return JsonResponse({"status": "ok"})
